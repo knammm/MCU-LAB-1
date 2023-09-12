30cb3671a665c7d869a863736721d5c7eb245fe2
Nam Nguyen <nam.nguyenolkmphy@hcmut.edu.vn>
2023-09-13 00:06:27 +0700

    Implemented clearAllClock function

--- Source/Core/Src/main.c
+++ Source/Core/Src/main.c
@@ -81,6 +81,21 @@ void clock(int signal){
 	if(signal == 11) HAL_GPIO_WritePin(LED_11_GPIO_Port, LED_11_Pin, SET);
 	if(signal == 12) HAL_GPIO_WritePin(LED_12_GPIO_Port, LED_12_Pin, SET);
 }
+
+void clearAllClock(){
+	HAL_GPIO_WritePin(LED_1_GPIO_Port, LED_1_Pin, RESET);
+	HAL_GPIO_WritePin(LED_2_GPIO_Port, LED_2_Pin, RESET);
+	HAL_GPIO_WritePin(LED_3_GPIO_Port, LED_3_Pin, RESET);
+	HAL_GPIO_WritePin(LED_4_GPIO_Port, LED_4_Pin, RESET);
+	HAL_GPIO_WritePin(LED_5_GPIO_Port, LED_5_Pin, RESET);
+	HAL_GPIO_WritePin(LED_6_GPIO_Port, LED_6_Pin, RESET);
+	HAL_GPIO_WritePin(LED_7_GPIO_Port, LED_7_Pin, RESET);
+	HAL_GPIO_WritePin(LED_8_GPIO_Port, LED_8_Pin, RESET);
+	HAL_GPIO_WritePin(LED_9_GPIO_Port, LED_9_Pin, RESET);
+	HAL_GPIO_WritePin(LED_10_GPIO_Port, LED_10_Pin, RESET);
+	HAL_GPIO_WritePin(LED_11_GPIO_Port, LED_11_Pin, RESET);
+	HAL_GPIO_WritePin(LED_12_GPIO_Port, LED_12_Pin, RESET);
+}
 /* USER CODE END 0 */
 
 /**
@@ -112,7 +127,6 @@ int main(void)
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
   /* USER CODE BEGIN 2 */
-  // EX7 begin
   int counter = 1;
   /* USER CODE END 2 */
 
